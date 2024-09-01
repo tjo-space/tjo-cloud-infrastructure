609e1089b11ae516cae68b2ee055b6df27f699ee
Tine <tine@tjo.space>
2024-09-01 10:10:21 +0200

    feat: working on it

--- proxmox.tjo.cloud/configuration.nix
+++ proxmox.tjo.cloud/configuration.nix
@@ -1,8 +1,45 @@
-{ lib, pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    "${toString modulesPath}/profiles/qemu-guest.nix"
+  ];
+
   system.stateVersion = "24.05";
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    autoResize = true;
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/ESP";
+    fsType = "vfat";
+  };
+
+  boot.growPartition = true;
+  boot.kernelParams = [ "console=ttyS0" ];
   boot.loader.systemd-boot.enable = true;
+  #boot.loader.grub.device = "nodev";
+  #boot.loader.grub.efiSupport = true;
+  #boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.timeout = 0;
+
+  system.build.qcow2 = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    name = "nixos";
+    diskSize = "auto";
+    format = "qcow2-compressed";
+    partitionTableType = "efi";
+    copyChannel = true;
+    configFile = pkgs.writeText "configuration.nix" (pkgs.lib.readFile ./configuration.nix);
+  };
 
   services.qemuGuest.enable = true;
 
--- proxmox.tjo.cloud/flake.nix
+++ proxmox.tjo.cloud/flake.nix
@@ -1,70 +1,18 @@
 {
+  description = "Basic NixOS qcow2 image with CloudInit for Proxmox";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    xc = {
-      url = "github:joerdav/xc";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-
   outputs =
+    { self, nixpkgs }:
     {
-      nixpkgs,
-      nixos-generators,
-      xc,
-      ...
-    }:
-    let
-      pkgsForSystem =
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ (final: prev: { xc = xc.packages.${system}.xc; }) ];
+      nixosConfigurations = {
+        build-qcow2 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+          ];
         };
-      allVMs = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forAllVMs =
-        f:
-        nixpkgs.lib.genAttrs allVMs (
-          system:
-          f {
-            inherit system;
-            pkgs = pkgsForSystem system;
-          }
-        );
-    in
-    {
-      packages = forAllVMs (
-        { system, pkgs }:
-        {
-          vm = nixos-generators.nixosGenerate {
-            format = "qcow-efi";
-            system = system;
-
-            specialArgs = {
-              pkgs = pkgs;
-            };
-
-            modules = [
-              # Pin nixpkgs to the flake input, so that the packages installed
-              # come from the flake inputs.nixpkgs.url.
-              (
-                { ... }:
-                {
-                  nix.registry.nixpkgs.flake = nixpkgs;
-                }
-              )
-              # Apply the rest of the config.
-              ./configuration.nix
-            ];
-          };
-        }
-      );
+      };
     };
 }
