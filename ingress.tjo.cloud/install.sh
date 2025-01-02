95a9163aa35202a9e69fcef7b6534c3aab0416bc
Tine <tine@tjo.space>
2025-01-02 16:48:50 +0100

    fix(ingress.tjo.cloud): copy binary

--- ingress.tjo.cloud/install.sh
+++ ingress.tjo.cloud/install.sh
@@ -80,6 +80,7 @@ systemctl restart alloy
 ##
 echo "== Configure Dyndns"
 cp root/etc/systemd/system/dyndns.service /etc/systemd/system/dyndns.service
+cp root/usr/local/bin/dyndns /usr/local/bin/dyndns
 cp -r root/etc/default/dyndns /etc/default/dyndns
 {
   echo "DIGITALOCEAN_TOKEN=${DIGITALOCEAN_TOKEN}"
