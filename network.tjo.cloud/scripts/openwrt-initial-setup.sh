9dac00de9f745e32d1bf92e2efc6970adee6880b
Tine <tine@tjo.space>
2025-03-23 18:55:22 +0100

    feat: network init script

--- network.tjo.cloud/scripts/openwrt-initial-setup.sh
+++ network.tjo.cloud/scripts/openwrt-initial-setup.sh
@@ -10,7 +10,7 @@ opkg install qemu-ga
 # Ref: https://github.com/adyanth/openwrt-tailscale-enabler/tree/v1.60.0-e428948-autoupdate
 opkg install ca-bundle kmod-tun iptables-nft kmod-ipt-conntrack kmod-ipt-conntrack-extra kmod-ipt-conntrack-label kmod-ipt-nat kmod-nft-nat
 
-wget https://github.com/adyanth/openwrt-tailscale-enabler/releases/download/v1.60.0-e428948-autoupdate/openwrt-tailscale-enabler-v1.60.0-e428948-autoupdate.tgz -o /tmp/tailscale-enabler.tgz
+wget https://github.com/adyanth/openwrt-tailscale-enabler/releases/download/v1.60.0-e428948-autoupdate/openwrt-tailscale-enabler-v1.60.0-e428948-autoupdate.tgz -O /tmp/tailscale-enabler.tgz
 
 tar x -zvC / -f /tmp/tailscale-enabler.tgz
 
