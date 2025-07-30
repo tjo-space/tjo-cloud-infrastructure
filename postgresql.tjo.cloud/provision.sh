ba9459728fc9ac7e761627ee1e6c36bf28082f40
Tine <tine@tjo.space>
2025-07-30 10:29:03 +0200

    feat(postgresql.tjo.cloud): ssl fixes

--- postgresql.tjo.cloud/provision.sh
+++ postgresql.tjo.cloud/provision.sh
@@ -15,15 +15,23 @@ DEBIAN_FRONTEND=noninteractive apt install -y \
   barman \
   barman-cli \
   postgresql-16 \
-  restic \
-  lego
+  restic
 
-# Grafana Alloy
+echo "=== Grafana Alloy"
 mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor >/etc/apt/keyrings/grafana.gpg
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" >/etc/apt/sources.list.d/grafana.list
 apt update -y
 apt install -y alloy
+
+echo "=== Lego"
+LEGO_VERSION="v4.25.1"
+LEGO_TMP_DIR=$(mktemp -d)
+pushd "$LEGO_TMP_DIR"
+curl -sL "https://github.com/go-acme/lego/releases/download/${LEGO_VERSION}/lego_${LEGO_VERSION}_linux_$(dpkg --print-architecture).tar.gz" | tar xvz --strip-components=1
+install lego /usr/local/bin/lego
+popd
+rm -rf "$LEGO_TMP_DIR"
 
 echo "=== Generating Age Key"
 mkdir -p /etc/age
