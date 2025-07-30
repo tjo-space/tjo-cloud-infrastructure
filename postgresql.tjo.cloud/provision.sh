4c5e497419dfaf65948727f9d0b4d3423d3bf93b
Tine <tine@tjo.space>
2025-07-30 10:32:19 +0200

    feat(postgresql.tjo.cloud): ssl fixes

--- postgresql.tjo.cloud/provision.sh
+++ postgresql.tjo.cloud/provision.sh
@@ -28,7 +28,7 @@ echo "=== Lego"
 LEGO_VERSION="v4.25.1"
 LEGO_TMP_DIR=$(mktemp -d)
 pushd "$LEGO_TMP_DIR"
-curl -sL "https://github.com/go-acme/lego/releases/download/${LEGO_VERSION}/lego_${LEGO_VERSION}_linux_$(dpkg --print-architecture).tar.gz" | tar xvz --strip-components=1
+curl -sL "https://github.com/go-acme/lego/releases/download/${LEGO_VERSION}/lego_${LEGO_VERSION}_linux_$(dpkg --print-architecture).tar.gz" | tar xvz
 install lego /usr/local/bin/lego
 popd
 rm -rf "$LEGO_TMP_DIR"
