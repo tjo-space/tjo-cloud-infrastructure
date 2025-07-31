8b8d97bb47786288280d995db5c46b75c9975c3e
Tine <tine@tjo.space>
2025-07-31 11:39:32 +0200

    feat(k8s.tjo.cloud): rbac and argo upgrade

--- k8s.tjo.cloud/modules/cluster-components/argocd.tf
+++ k8s.tjo.cloud/modules/cluster-components/argocd.tf
@@ -2,52 +2,85 @@ resource "helm_release" "argocd" {
   name            = "argo-cd"
   chart           = "argo-cd"
   repository      = "https://argoproj.github.io/argo-helm"
-  version         = "7.8.27"
+  version         = "8.2.4"
   namespace       = kubernetes_namespace.tjo-cloud.metadata[0].name
   atomic          = true
   cleanup_on_fail = true
 
-  values = [<<EOF
-    crds:
-      install: false
-
-    global:
-      domain: "argocd.k8s.tjo.cloud"
-
-    configs:
-      params:
-        server.insecure: true
-      cm:
-        admin.enabled: false
-        statusbadge.enabled: true
-        dex.config: |
-          connectors:
-            - name: "id.tjo.cloud"
-              id: "id-tjo-cloud"
-              type: "oidc"
-              config:
-                issuer: "${var.oidc_issuer_url}"
-                clientID: "${var.oidc_client_id}"
-                clientSecret: "null"
-                insecureEnableGroups: true
-                scopes: ["openid", "profile", "email", "groups"]
-      ssh:
-        extraHosts: |
+  values = [yamlencode({
+    crds = {
+      install = false
+    }
+    global = {
+      domain = "argocd.k8s.tjo.cloud"
+      dualstack = {
+        ipFamilyPolicy = "RequireDualStack"
+        ipFamilies     = ["IPv4", "IPv6"]
+      }
+    }
+    configs = {
+      params = {
+        server = {
+          insecure = true
+        }
+      }
+      rbac = {
+        policy = {
+          csv = <<EOF
+          g, cloud.tjo.k8s/admin, role:admin
+          g, cloud.tjo.k8s/read-only, role:readonly
+          EOF
+        }
+      }
+      cm = {
+        admin       = { enabled = false }
+        statusbadge = { enabled = true }
+        dex = {
+          config = yamlencode({
+            connectors = [
+              {
+                name = "id.tjo.cloud"
+                id   = "id-tjo-cloud"
+                type = "oidc"
+                config = {
+                  issuer               = var.oidc_issuer_url
+                  clientID             = var.oidc_client_id
+                  clientSecret         = "null"
+                  insecureEnableGroups = true
+                  scopes               = ["openid", "profile", "email", "groups"]
+                }
+              }
+            ]
+          })
+        }
+      }
+      ssh = {
+        extraHosts = <<EOF
           code.tjo.space ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJqsHFLpLlH/RXKJA9YpCxyqfZtZeKfuYhIHfL7wziZSew7Cff6Pt7OyXXq9QtqYAIjXFJD7gLDFBFCQFBg8yvFt9rOcI8yaIlJwKaCVWVqVfyKI7W0hbuUQyGYdgUVS/A71YWIlJsqnMc95ddFK31nmOuoFnayKlB9jpPkYouLuRJ4nlR+mNiUkFGBHq0LD7lPth3djxgyHQteNApQ/zMWdzgnm4x+nOsDZ8DRZ5hsr7jfmmjjqNBHunHJuwP9BiLrzqCpWM/iCsCNqamOV9jIt+F+nJg9622qULWzeHnclBMlawBmuyGSfmk+nCYYW8kGLzKVryy6w7BfcRg/7e6/YujnlVxPzSyFqFgNlaFkY/PuK3nBjv7AjBgPkj0A8uiiP/wuMN4Kd9h6CYozM02ECMlGu1aCCIaG/Xog6UDb1R+bgdvchBIOOx04KomYZblB2XSv9NVE+UIBNBKGEK2FgA1gV+DUizK/jm10PGZDtGXzlzvMxY/PZiFdf6G8VLVt7nNf//jAQRjl+3bPFVLR3DLqpVGxd48nuvljW1jbB6uGIPo/nbzzHGKz7mjX2QwHynb6cwjug55zNxPVCSIBmnye1fYQFS8ESIfP1SXzdoKSB78uinU7MEeCGWh7hFo53OvrocOzHnUMmsy4VaGZlbUdSMMU6lwO0bDg+CYnw==
-
-    controller:
-      replicas: 1
-
-    server:
-      replicas: 2
-
-    repoServer:
-      replicas: 2
-
-    applicationSet:
-      replicas: 2
-  EOF
-  ]
+          EOF
+      }
+      clusterCredentials = {
+        "k8s.tjo.cloud" = {
+          server      = "https://kubernetes.default.svc"
+          labels      = {}
+          annotations = {}
+          config      = {}
+        }
+      }
+    }
+    controller = {
+      replicas = 1
+    }
+    server = {
+      replicas = 2
+    }
+    repoServer = {
+      replicas = 2
+    }
+    applicationSet = {
+      replicas = 2
+    }
+  })]
 }
 
 resource "kubernetes_manifest" "argocd-http-route" {
--- k8s.tjo.cloud/modules/cluster-core/rbac.tf
+++ k8s.tjo.cloud/modules/cluster-core/rbac.tf
@@ -1,17 +1,35 @@
 resource "kubernetes_cluster_role_binding" "oidc-admins" {
   metadata {
-    name = "id-tjo-space:admins"
+    name = "cloud.tjo.k8s-admin"
   }
 
   subject {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Group"
-    name      = "oidc:groups:k8s.tjo.cloud admin"
+    name      = "oidc:groups:cloud.tjo.k8s/admin"
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
     name      = "cluster-admin"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "oidc-read-only" {
+  metadata {
+    name = "cloud.tjo.k8s-read-only"
+  }
+
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Group"
+    name      = "oidc:groups:cloud.tjo.k8s/read-only"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "view"
   }
 }
